5fe5b9f843865b0ecc789e3ffec18b1c1cfd40f3
yonghk423 <yonghk423423@gmail.com>
2022-09-27 14:35:29 +0900

    study

--- src/components/Main.js
+++ src/components/Main.js
@@ -55,5 +55,48 @@ const Section = styled.section`
         width: 100%;
         position: absolute;
         top:0;
+        z-index: 3;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 1rem;
+        .title {
+            color : white;
+            h1 {
+                font-size: 3rem;
+                letter-spacing: 0.2rem;
+            }
+            p {
+                text-align: center;
+                padding: 0 30vw;
+                margin-top: 0.5rem;
+                font-size: 1.2rem;
+            }
+        }
+        .search {
+            display: flex;
+            background-color: #ffffffce;
+            padding: 0.5rem;
+            border-radius: 0.5rem;
+            .container {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-direction: column;
+                padding: 0 1.5rem;
+                label {
+                    font-size: 1.1rem;
+                    color: #03045e;
+                }
+                input {
+                    background-color: transparent;
+                    border: none;
+                    text-align: center;
+                    color: black;
+                }
+            }
+        }
     }
 `;
