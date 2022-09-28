040bc1f0ac492359953ec69532224c07a6813dc8
yonghk423 <yonghk423423@gmail.com>
2022-09-28 14:28:18 +0900

    study

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,8 +4,55 @@ import avatarImage from "../assets/avatarImage.jpeg";
 
 const Testimonials = () => {
     return (
-        <Section>
-            
+        <Section id='testimonials'>
+            <div className='title'>
+                <h2>Happy Customers</h2>
+            </div>
+            <div className='testimonials'>
+                <div className='testimonial'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+                        asperiores eaque.
+                    </p>
+                    <div className='info'>
+                        <img src={avatarImage} alt='' />
+                        <div className='details'>
+                            <h4>Kishan Sheth</h4>
+                            <span>CEO - Shashaan Web Solutions</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='testimonials'>
+                <div className='testimonial'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+                        asperiores eaque.
+                    </p>
+                    <div className='info'>
+                        <img src={avatarImage} alt='' />
+                        <div className='details'>
+                            <h4>Kishan Sheth</h4>
+                            <span>CEO - Shashaan Web Solutions</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='testimonials'>
+                <div className='testimonial'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+                        asperiores eaque.
+                    </p>
+                    <div className='info'>
+                        <img src={avatarImage} alt='' />
+                        <div className='details'>
+                            <h4>Kishan Sheth</h4>
+                            <span>CEO - Shashaan Web Solutions</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Section>
     )
 }
