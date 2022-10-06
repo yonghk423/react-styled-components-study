540c274ac751598c7fe8f44b5b289b1bea85dc15
yonghk423 <yonghk423423@gmail.com>
2022-10-06 14:00:51 +0900

    study

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -22,7 +22,6 @@ const Testimonials = () => {
                         </div>
                     </div>
                 </div>
-            </div>
             <div className='testimonials'>
                 <div className='testimonial'>
                     <p>
@@ -53,10 +52,45 @@ const Testimonials = () => {
                     </div>
                 </div>
             </div>
+            </div>    
         </Section>
     )
 }
 
 export default Testimonials;
 
-const Section = styled.section``;
+const Section = styled.section`
+    margin: 5rem 0;
+    .title {
+        text-align: center;
+        margin-bottom: 2rem;
+    }
+    .testimonials {
+        display: flex;
+        justify-content: center;
+        margin: 0.2rem;
+        gap: 2rem;
+        .testimonial {
+            background-color: aliceblue;
+            padding: 2rem;
+            border-radius: 0.5rem;
+            box-shadow: rgba(100, 100, 111, 0,2) 0px 7px 29px 0px;
+            transition: 0.3s ease-in-out;
+            &:hover {
+                transform: translateX(0.4rem) translateY(-1rem);
+                box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+            }
+            .info {
+                display: flex;
+                justify-content: center;
+                gap: 1rem;
+                align-items: center;
+                margin-top: 1rem;
+                img {
+                    border-radius: 3rem;
+                    height: 3rem;
+                }
+            }
+        }        
+    }
+`;
