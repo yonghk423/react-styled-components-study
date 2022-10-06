d6b71a93a89ae42dda4a098b0586026b1a95090a
yonghk423 <yonghk423423@gmail.com>
2022-10-06 14:35:01 +0900

    study

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -38,4 +38,32 @@ const Footer = () => {
 export default Footer;
 
 const FooterContainer = styled.footer`
+    display: flex;
+    justify-content: space-evenly;
+    background-color: #d0d8ff;
+    border-radius: 0.5rem;
+    padding: 2.5rem;
+
+    ul {
+        display: flex;
+        list-style-type: none;
+        gap: 2rem;
+        li {
+            a {
+                text-decoration: none;
+                color: black;
+                transition: 0.3s ease-in-out;
+                &:hover {
+                    color: #302ce9;
+                }
+            }
+            svg {
+                font-size: 1.3rem;
+                transition: 0.3s ease-in-out;
+                &:hover {
+                    color: #302ce9;
+                }
+            }
+        }
+    }
 `;
