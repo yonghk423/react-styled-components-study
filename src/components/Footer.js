0a274b0c19854d617ebe0470dc6d6e7dc08b8a29
yonghk423 <yonghk423423@gmail.com>
2022-10-06 14:24:16 +0900

    study

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+import { BsLinkedin, BsFacebook } from "react-icons/bs";
+import { AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
     return (
-        <div></div>
+        <FooterContainer>
+            <span>Copyright &copy; 2021 Travelo. All rights reserved</span>
+            <ul className='links'>
+                <li>
+                    <a href="#Main">Home</a>
+                </li>
+                <li>
+                    <a href="#services">About</a>
+                </li>
+                <li>
+                    <a href="#recommend">Place</a>
+                </li>
+                <li>
+                    <a href="#testimonials">Testimonials</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <BsFacebook />
+                </li>
+                <li>
+                    <AiFillInstagram />
+                </li>
+                <li>
+                    <BsLinkedin />
+                </li>
+            </ul>
+        </FooterContainer>
     )
 }
 export default Footer;
+
+const FooterContainer = styled.footer`
+`;
